c9312d5d954188339984b35b4c90657d0f2e1396
Brian Brenner <bbrenne6@kent.edu>
2022-05-06 22:52:55 -0400

    added more comments

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ static HIDE_CURSOR: &str = "?25l";
 static RESET_CURSOR: &str = "H";
 
 // parse arg and return error if not passed a number
+// uses some and none to as a try/catch
 fn parse_arg(arg: Option<String>) -> u32 {
     match arg {
         Some(val) => {
@@ -26,7 +27,7 @@ fn parse_arg(arg: Option<String>) -> u32 {
     }
 }
 
-// simple conversion
+// simple conversion function that returns two unsigned 32 bit ints
 pub(crate) fn total_to_min_sec(total: u32) -> (u32, u32) {
     let _min = total / 60;
     let _sec = total - _min * 60;
@@ -46,7 +47,9 @@ fn main() {
     let total = minutes * 60 + seconds;
     let (_min, _sec): (u32, u32) = total_to_min_sec(total);
 
+    // program loop
     loop {
+        // time loop
         for counter in (1..=total).rev() {
             let (min, sec) = total_to_min_sec(counter);
             print!("{}{}{}{}{}{}count down: ",
@@ -55,7 +58,7 @@ fn main() {
                 ESC, RESET_CURSOR
             );
             println!("{:02}:{:02}", min, sec); // print macro, specify 2 digit space for printing
-            sleep(Duration::from_millis(1000 as u64));
+            sleep(Duration::from_millis(1000 as u64)); //equivalent to sleep(1)
         }
         break;
     }
