0c358d36159fadb53a34a3a2538ff478d91a432a
Brian Brenner <bbrenne6@kent.edu>
2022-05-06 21:07:15 -0400

    added comments to code

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ use std::thread::sleep;
 use std::env::args;
 use std::time::Duration;
 
+// setting asci codes for clearing screen 
 static ESC: &str = "\x1b[";
 static CLEAR: &str = "2J";
 static HIDE_CURSOR: &str = "?25l";
 static RESET_CURSOR: &str = "H";
 
+// parse arg and return error if not passed a number
 fn parse_arg(arg: Option<String>) -> u32 {
     match arg {
         Some(val) => {
@@ -24,6 +26,7 @@ fn parse_arg(arg: Option<String>) -> u32 {
     }
 }
 
+// simple conversion
 pub(crate) fn total_to_min_sec(total: u32) -> (u32, u32) {
     let _min = total / 60;
     let _sec = total - _min * 60;
@@ -34,6 +37,7 @@ pub(crate) fn total_to_min_sec(total: u32) -> (u32, u32) {
 fn main() {
     let mut options = args().skip(1);
 
+    // parse through given args and consolidate minutes and seconds
     let first_arg: Option<String> = options.next();
     let minutes = parse_arg(first_arg);
     let second_arg: Option<String> = options.next();
@@ -50,7 +54,7 @@ fn main() {
                 ESC, HIDE_CURSOR,
                 ESC, RESET_CURSOR
             );
-            println!("{:02}:{:02}", min, sec);
+            println!("{:02}:{:02}", min, sec); // print macro, specify 2 digit space for printing
             sleep(Duration::from_millis(1000 as u64));
         }
         break;
